c8c1e1a4acce5850b96cb60c7675db0adeebe9c3
RianHaryadi <rianharyadi89@gmail.com>
2025-08-19 13:44:33 +0700

    update

--- app/Livewire/LoginForm.php
+++ app/Livewire/LoginForm.php
@@ -27,7 +27,7 @@ class LoginForm extends Component
 
     public function render()
     {
-        return view('livewire.login-form')->layout('layouts.login');
+        return view('livewire.login-Form')->layout('layouts.login');
     }
 }
 
